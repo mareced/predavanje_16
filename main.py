5f3377f11c4e3e2f9596b5050f3e369a4e3cb27d
MarkoC <marko.cedilnik@gmail.com>
2019-11-07 17:53:33 +0100

    Dodali smo formo v kontakt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -10,6 +10,17 @@ def prva_stran():
 @app.route("/kontakt")
 def kontakt():
     return render_template("kontakt.html")
+
+@app.route("/poslji-sporocilo", methods=["POST"])
+def poslji_sporocilo():
+    zadeva = request.form.get("zadeva")
+    sporocilo = request.form.get("sporocilo")
+
+     # Toukaj bi shranili te spremenjljivki v bazo
+
+    print ("zadeva je: " + zadeva)
+    print ("Sporočilo je: " + sporocilo)
+    return "Hvala za poslano zadevo: " + zadeva
 
 
 if __name__ == '__main__':
