78fa6be0dbcf82017eb3525ec0b3f51a671e9e7f
MarkoC <marko.cedilnik@gmail.com>
2019-11-14 18:09:13 +0100

    Dodal shranjevanje komentarjev

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, make_response
+from modeli import Komentar, db
 
 
 app = Flask(__name__)
+db.create_all()
 
 @app.route("/")
 def prva_stran():
     ime = request.cookies.get("ime")
-    return render_template ("prva_stran.html", ime=ime)
+
+    komentarji = db.query(Komentar).all()
+    return render_template ("prva_stran.html", ime=ime, komentarji=komentarji)
 
 @app.route("/kontakt")
 def kontakt():
@@ -30,6 +34,14 @@ def poslji_komentar():
     vsebina_komentarja = request.form.get("vsebina")
 
     #Tukaj se bo shranil komentar v podatkovno bazo
+
+    komentar = Komentar(
+        avtor=request.cookies.get("ime"),
+        vsebina=vsebina_komentarja)
+
+    db.add(komentar)
+
+    db.commit()
 
     return redirect("/")
 
---
+++ modeli.py
@@ -0,0 +1,9 @@
+import os
+from sqla_wrapper import SQLAlchemy
+
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///podatkovna-baza.sqlite"))
+
+class Komentar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    avtor = db.Column(db.String)
+    vsebina = db.Column(db.String)
